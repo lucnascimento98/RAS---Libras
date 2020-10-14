74cdb60adef15aefa1e16971560b07c277fc3cd6
Lucas Serrano <72429343+lucas-serrano@users.noreply.github.com>
2020-10-14 05:54:57 -0300

    Teste de Rede Neural em Arquivo Único

--- Serrano/Rede_neural.py
+++ Serrano/Rede_neural.py
@@ -1,8 +1,18 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential # Modelo Sequencial
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard # Para visualização do Modelo da Rede Neural
 import pickle
 import numpy as np
+
+import time
+
+NAME = 'Verificação_de_letras-cnn-64x2-{}'.format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir = 'logs/{}'.format(NAME)) # Para ter uma visualização do treinamento da rede neural
+
+#  tensorboard --logdir=logs/
+
 
 pickle_in = open("X.pickle","rb")
 X = np.array(pickle.load(pickle_in))
@@ -27,12 +37,13 @@ model.add(MaxPooling2D(pool_size=(2,2)))
 # Terceira Layer
 model.add(Flatten()) #Deixa a rede neural em 2D
 model.add(Dense(64))
+model.add(Activation('relu'))
 
 # Layer de saída
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 model.compile(loss = "binary_crossentropy", optimizer = "adam", metrics = ['accuracy'])
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.1)
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks = [tensorboard])
 
 model.save('treinando_rede.model') # Salvando Rede Neural
