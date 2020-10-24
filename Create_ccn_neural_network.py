4422f648d501664a15ec09f712f36c574e0f3b64
Lucas Serrano <72429343+lucas-serrano@users.noreply.github.com>
2020-10-24 11:01:33 -0300

    Program corrections

--- Create_ccn_neural_network.py
+++ Create_ccn_neural_network.py
@@ -24,6 +24,7 @@ def create_training_data():
                 img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE) #Cada imagem também é convertida na escala de cinza
                 new_array = cv2.resize(img_array,(IMG_SIZE,IMG_SIZE)) #Redimensionando a imagem
                 training_data.append([new_array,class_num]) #Inserindo a imagem e sua classificação
+                print(category,img)
             except Exception as e: #Erro que pode ocorrer ao importar imagens
                 pass
 
@@ -41,6 +42,9 @@ for features, label in training_data:
 
 X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1) # -1: Quantas features tem, IMG_SIZE x IMG_SIZE: tamanho da imagem, 1: indica que o canal de cores é só o cinza (3 se RGB)
 
+X = np.array(X/255.0) # Ajustando valores da variação da escala de cinza entre 0 e 1
+y = np.array(y) # Transformando de lista para matriz
+
 #####################################################################################################################
 
 NAME = 'Verificação_de_letras-cnn-64x2-{}'.format(int(time.time()))
@@ -49,28 +53,27 @@ tensorboard = TensorBoard(log_dir = 'logs/{}'.format(NAME)) # Para ter uma visua
 
 X = np.array(X/255.0) #Simplificando coloração de Pixels para valores entre 0 e 1
 
-# Primeira Layer
-model = Sequential()
-model.add(Conv2D(64,(3,3), input_shape = X.shape[1:]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model = Sequential([Conv2D(64,(2,2), activation='relu', input_shape = X.shape[1:]), # Primeira camada
+                    MaxPooling2D(pool_size=(2,2)),
 
-# Segunda Layer
-model = Sequential()
-model.add(Conv2D(64,(3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+                    Conv2D(128,(2,2), activation='relu'), # Segunda camada
+                    MaxPooling2D(pool_size=(2,2)),
 
-# Terceira Layer
-model.add(Flatten()) #Deixa a rede neural em 2D
-model.add(Dense(64))
-model.add(Activation('relu'))
+                    Conv2D(256,(2,2), activation='relu'), # Terceira camada
+                    MaxPooling2D(pool_size=(2,2)),
 
-# Layer de saída
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
+                    Conv2D(256,(2,2), activation='relu'), # Quarta camada
+                    MaxPooling2D(pool_size=(2,2)),
 
-model.compile(loss = "binary_crossentropy", optimizer = "adam", metrics = ['accuracy'])
-model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks = [tensorboard])
+                    Flatten(),
 
-model.save('treinando_rede.model') # Salvando Rede Neural
+                    Dense(512, activation='relu'), # Quinta camada
+
+                    Dense(22, activation='softmax')]) # Camada de Saída #APRESENTA PROBLEMAS NA SAÍDA
+
+# model.summary()
+adam = tf.keras.optimizers.Adam(learning_rate=0.001)
+model.compile(optimizer= adam, loss='categorical_crossentropy', metrics=['acc'])
+model.fit(X, y, batch_size=32, epochs=1, validation_split=0.1, callbacks = [tensorboard])
+
+model.save('output\\treinando_rede_all.model') # Salvando Rede Neural
