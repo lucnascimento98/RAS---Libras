a341675472e9cc29a0e2fa1ef0271daba275b2a1
Lucas Serrano <72429343+lucas-serrano@users.noreply.github.com>
2020-11-02 20:22:14 -0300

    Updating files

--- legacy_files/Create_neural_cnn.py
+++ legacy_files/Create_neural_cnn.py
@@ -19,14 +19,14 @@ y = np.array(pickle.load(pickle_in))
 
 X = np.array(X/255.0) #Simplificando coloração de Pixels para valores entre 0 e 1
 
-# Primeira Layer
 model = Sequential()
+
+# Primeira Layer
 model.add(Conv2D(64,(3,3), input_shape = X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 
 # Segunda Layer
-model = Sequential()
 model.add(Conv2D(64,(3,3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -43,4 +43,4 @@ model.add(Activation('sigmoid'))
 model.compile(loss = "binary_crossentropy", optimizer = "adam", metrics = ['accuracy'])
 model.fit(X, y, batch_size=32, epochs=10, validation_split=0.1, callbacks = [tensorboard])
 
-model.save('treinando_rede.model') # Salvando Rede Neural
+model.save('output\\treinando_rede.model') # Salvando Rede Neural
