632b257322057a9ea63e13bc5ebb47afdd801448
IrisGiglioti <irisamorozini@yahoo.com.br>
2020-11-02 20:32:00 -0300

    Códigos 4° sprint

---
+++ Teste_Background_Subtraction.py
@@ -0,0 +1,51 @@
+from __future__ import print_function
+import cv2
+import argparse
+
+parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
+                                              OpenCV. You can process both videos and images.')
+parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.', default='vtest.avi')
+parser.add_argument('--algo', type=str, help='Background subtraction method (KNN, MOG2).', default='MOG2')
+args = parser.parse_args()
+
+## [create]
+#create Background Subtractor objects
+if args == 'MOG2':
+    backSub = cv2.createBackgroundSubtractorMOG2()
+else:
+    backSub = cv2.createBackgroundSubtractorKNN()
+## [create]
+
+## [capture]
+capture = cv2.VideoCapture(cv2.samples.findFileOrKeep(args))
+if not capture.isOpened:
+    print('Unable to open: ' + args)
+    exit(0)
+## [capture]
+
+while True:
+    ret, frame = capture.read()
+    if frame is None:
+        break
+
+    ## [apply]
+    #update the background model
+    fgMask = backSub.apply(frame)
+    ## [apply]
+
+    ## [display_frame_number]
+    #get the frame number and write it on the current frame
+    cv2.rectangle(frame, (10, 2), (100,20), (255,255,255), -1)
+    cv2.putText(frame, str(capture.get(cv2.CAP_PROP_POS_FRAMES)), (15, 15),
+               cv2.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
+    ## [display_frame_number]
+
+    ## [show]
+    #show the current frame and the fg masks
+    cv2.imshow('Frame', frame)
+    cv2.imshow('FG Mask', fgMask)
+    ## [show]
+
+    keyboard = cv2.waitKey(30)
+    if keyboard == 'q' or keyboard == 27:
+        break
