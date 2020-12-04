549c74de708229801ce3a39fac84cec8b9aa768f
Lucas Serrano <72429343+lucas-serrano@users.noreply.github.com>
2020-12-04 16:27:10 -0300

    updating

---
+++ RAS_TESTE_SCREEN/Tela/Testar_screen.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QLabel, QComboBox, QWidget, QVBoxLayout
+from PyQt5 import QtGui, QtCore, QtWidgets
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
+
+import cv2
+import numpy as np
